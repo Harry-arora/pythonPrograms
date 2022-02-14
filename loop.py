05a789233d64f60e28aca720cd022ec73f5ed8bc
Harry-arora <99471403+Harry-arora@users.noreply.github.com>
2022-02-14 23:42:34 +0530

    Update loop.py
    examples of loops

---
+++ loop.py
@@ -0,0 +1,24 @@
+# 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
+
+# Program to find the sum of all numbers stored in a list
+
+# List of numbers
+numbers = [12, 45, 78, 56, 45, 23]
+
+# variable to store the sum
+sum = 0
+
+# iterate over the list
+for val in numbers:
+    sum = sum+val
+
+print("The sum is", sum)
+
+# for loop with if else conditions
+
+digits = [0, 1, 5]
+
+for i in digits:
+    print(i)
+else:
+    print("No items left.")
