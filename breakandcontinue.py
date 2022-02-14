bbc1d3bbeef3ed87333d38b8ff099cb9f3711e43
Harry-arora <99471403+Harry-arora@users.noreply.github.com>
2022-02-15 00:06:24 +0530

    Update breakandcontinue.py
    break n continue

---
+++ breakandcontinue.py
@@ -0,0 +1,17 @@
+# Use of break statement inside the loop
+
+for val in "harshit":
+    if val == "s":
+        break
+    print(val)
+
+print("The end")
+
+# Program to show the use of continue statement inside loops
+
+for val in "harshit":
+    if val == "i":
+        continue
+    print(val)
+
+print("The end")
